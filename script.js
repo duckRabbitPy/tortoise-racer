90c53b3db9e8c87a45e2bdd2a03fbe2c37f3bb23
duckRabbitPy <duck.rabbit.python@gmail.com>
2021-07-27 17:33:45 +0100

    deploy on surge

---
+++ script.js
@@ -0,0 +1,79 @@
+// const tortoiseOne = document.querySelector(".tortoise-one");
+// const tortoiseTwo = document.querySelector(".tortoise-two");
+const message = document.querySelector(".message");
+const goButton = document.querySelector(".race-go");
+const replayButton = document.querySelector(".replay");
+const addButton = document.querySelector(".add-racer");
+
+const raceArea = document.querySelector(".race-area");
+
+let move;
+let raceOver = false;
+let raceArr = [];
+let currNum = 2;
+
+goButton.addEventListener("click", () => {
+  raceArr = [];
+  const racers = document.querySelectorAll(".tortoise");
+  racers.forEach((racer) => {
+    let distance = 0;
+    raceArr.push([racer, distance]);
+  });
+
+  if (!raceOver) {
+    move = setInterval(moveTortoise, 500);
+  }
+});
+
+function moveTortoise() {
+  const racers = document.querySelectorAll(".tortoise");
+
+  racers.forEach((tortoise, index) => {
+    let newDistance = raceArr[index][1] + getRandomInt(10);
+    raceArr[index][1] = newDistance;
+    tortoise.style.transform = `translateX(${newDistance}vw)`;
+    if (newDistance > 70) {
+      clearInterval(move);
+
+      sorted = raceArr.sort(compare);
+      winner = sorted[0][0];
+      winner.id = "winner";
+      alert(`game over!`);
+    }
+  });
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+replayButton.addEventListener("click", () => {
+  raceOver = false;
+  clearInterval(move);
+  const racers = document.querySelectorAll(".tortoise");
+  racers.forEach((tortoise) => {
+    tortoise.id = "";
+    tortoise.style.transform = `translateX(${0}vw)`;
+  });
+
+  message.innerHTML = "Let's go!";
+});
+
+addButton.addEventListener("click", () => {
+  currNum += 1;
+  const newContent = document.createElement("div");
+  newContent.classList.add("tortoise");
+  newContent.innerText = currNum;
+  raceArea.appendChild(newContent);
+  document.body.insertBefore(tortoiseTwo, raceArea);
+});
+
+function compare(a, b) {
+  if (b[1] < a[1]) {
+    return -1;
+  }
+  if (b[1] > a[1]) {
+    return 1;
+  }
+  return 0;
+}
